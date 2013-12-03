e5e278cf05a2f1112d66e63c255ddfabb7ca4235
Jukka Suomela <jukka.suomela@iki.fi>
2013-12-03 23:18:07 +0200

    Bug fix.

--- src/colour-cv-compact.cc
+++ src/colour-cv-compact.cc
@@ -23,8 +23,8 @@ static void reduce12_6(unsigned n, const unsigned *p, col_t *c, const col_t *col
     Timer t {"colour 12 -> 6"};
     unsigned n2 {n/2};
     FOREACH(i, n2, c[i] = pack2(
-        reduce(unpack2(c[i], 0), get2(cold, p[2*i + 0])),
-        reduce(unpack2(c[i], 1), get2(cold, p[2*i + 1]))
+        reduce12_6(unpack2(cold[i], 0), get2(cold, p[2*i + 0])),
+        reduce12_6(unpack2(cold[i], 1), get2(cold, p[2*i + 1]))
     ));
 }
 
@@ -32,10 +32,10 @@ static void reduce6_4(unsigned n, const unsigned *p, col_t *c, const col_t *cold
     Timer t {"colour 6 -> 4"};
     unsigned n4 {n/4};
     FOREACH(i, n4, c[i] = pack4(
-        reduce(unpack2(c[2*i + 0], 0), get2(cold, p[4*i + 0])),
-        reduce(unpack2(c[2*i + 0], 1), get2(cold, p[4*i + 1])),
-        reduce(unpack2(c[2*i + 1], 2), get2(cold, p[4*i + 2])),
-        reduce(unpack2(c[2*i + 1], 3), get2(cold, p[4*i + 3]))
+        reduce6_4(unpack2(cold[2*i + 0], 0), get2(cold, p[4*i + 0])),
+        reduce6_4(unpack2(cold[2*i + 0], 1), get2(cold, p[4*i + 1])),
+        reduce6_4(unpack2(cold[2*i + 1], 0), get2(cold, p[4*i + 2])),
+        reduce6_4(unpack2(cold[2*i + 1], 1), get2(cold, p[4*i + 3]))
     ));
 }
 
