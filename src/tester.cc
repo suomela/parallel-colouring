62836cf76ec58f7afb3a19e2f8c8118503d8b2f1
Jukka Suomela <jukka.suomela@iki.fi>
2013-12-06 15:42:55 +0200

    Instance generation that can be parallelised.

--- src/tester.cc
+++ src/tester.cc
@@ -1,4 +1,5 @@
 #include "tester.h"
+#include <cassert>
 #include <random>
 #include "colour.h"
 #include "colour-interface.h"
@@ -6,6 +7,7 @@
 #include "timer.h"
 
 constexpr col_t C {3};
+constexpr unsigned MAXPAR {32};
 
 Tester::Tester(unsigned bits_)
  : n {1U << bits_}, mask {n - 1},
@@ -14,18 +16,24 @@ Tester::Tester(unsigned bits_)
 
 void Tester::gen_input() {
     Timer t{"generate input"};
-    std::mt19937 rng {1};
-    static_assert(rng.min() == 0, "rng.min");
-    static_assert(rng.max() == std::numeric_limits<unsigned>::max(), "rng.max");
+    assert(n % MAXPAR == 0);
+    unsigned part {n/MAXPAR};
+    for (unsigned j {0}; j < MAXPAR; ++j) {
+        std::mt19937 rng {j+1};
+        static_assert(rng.min() == 0, "rng.min");
+        static_assert(rng.max() == std::numeric_limits<unsigned>::max(), "rng.max");
+        unsigned start {j * part};
+        unsigned end {start + part};
+        for (unsigned i {start}; i < end; ++i) {
+            unsigned x {static_cast<unsigned>(rng()) & mask};
+            if (x == i) {
+                x = 0;
+            }
+            p[i] = x;
+        }
+    }
     p[0] = 1;
     p[1] = 0;
-    for (unsigned i {2}; i < n; ++i) {
-        unsigned x {static_cast<unsigned>(rng()) & mask};
-        if (x == i) {
-            x = 0;
-        }
-        p[i] = x;
-    }
 }
 
 void Tester::do_colour() {
